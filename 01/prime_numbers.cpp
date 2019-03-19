1feaa012fa8ae1e9523aaa0d7ce81776df890160
Anton <savchenko_a@icloud.com>
2019-03-19 09:31:35 +0300

    hw_1 bugfix_1

--- 01/prime_numbers.cpp
+++ 01/prime_numbers.cpp
@@ -3,50 +3,47 @@
 #include <algorithm>
 #include "numbers.dat"
 
-
-
-int binarySearchInData(const int Data[], int num_to_search, int first, int last)
+int binarySearch(const int Data[], int num_to_search, int first, int last, bool first_entry)
 {
+    /*
+    бинарный поиск по Data, возвращает:
+    индекс первого вхождения, если first_entry == true
+    индекс последнего вхождения, если first_entry == false
+    */
+    
     if (first >= last)
         return -1;
     int mid = first + (last - first) * 0.5;
     int mid_element = Data[mid];
-    if (Data[first] == num_to_search)
-        return first;
     if (mid_element == num_to_search)
     {
-        if (mid == first+1)
-            return mid;
+        if (first_entry)
+        {
+            while (mid_element == num_to_search)
+                mid_element = Data[--mid];
+            return mid + 1;
+        }
         else
-            return binarySearchInData(Data, num_to_search, first, mid+1);
+        {
+            while (mid_element == num_to_search)
+                mid_element = Data[++mid];
+            return mid - 1;
+        }
     }
     else if (mid_element > num_to_search)
-        return binarySearchInData(Data, num_to_search, first, mid);
-    else return binarySearchInData(Data, num_to_search, mid+1, last);
+        return binarySearch(Data, num_to_search, first, mid, first_entry);
+    else return binarySearch(Data, num_to_search, mid+1, last, first_entry);
 }
 
-int binarySearchInDataLast(const int Data[], int num_to_search, int first, int last)
-{
-    if (first >= last)
-        return -1;
-    int mid = first + (last - first) * 0.5;
-    int mid_element = Data[mid];
-    if (Data[last-1] == num_to_search)
-        return last-1;
-    if (mid_element == num_to_search)
-    {
-        if (mid == last-2)
-            return mid;
-        else
-            return binarySearchInDataLast(Data, num_to_search, mid, last-1);
-    }
-    else if (mid_element > num_to_search)
-        return binarySearchInDataLast(Data, num_to_search, first, mid);
-    else return binarySearchInDataLast(Data, num_to_search, mid+1, last);
-}
+
 
 std::vector<bool> sieveErato (int max_element)
 {
+    /*
+    решето Эратосфена заполняется до max_element
+    true - число простое, false - составное
+    */
+    
     std::vector<bool> sieve (max_element, true);
     sieve[0] = sieve[1] = false;
     for(long long i = 2; i <= max_element; i++)
@@ -66,14 +63,12 @@ int main(int args, char* argv[])
     if (args == 1 || args % 2 == 0)
         return -1;
     
-    std::vector<int> borders;
+    std::vector<int> borders (args - 1);
     
-    for (int i = 1; i < args; ++i)
-    {
-        int v = std::stoi(argv[i]);
-        borders.push_back(v);
-    }
+    for (int i = 0; i < args - 1; i++)
+        borders[i] = std::atoi(argv[i + 1]);
     
+    // определение max аргумента командной строки, чтобы построить решето до этого значения
     int max_in_pairs = *std::max_element(std::begin(borders), std::end(borders));
     std::vector<bool> sieve = sieveErato(max_in_pairs);
     
@@ -81,8 +76,8 @@ int main(int args, char* argv[])
     {
         int counter = 0;
         
-        int left_border = binarySearchInData(Data, borders[pair], 0, Size);
-        int right_border = binarySearchInDataLast(Data, borders[pair + 1], 0, Size);
+        int left_border = binarySearch(Data, borders[pair], 0, Size, true);
+        int right_border = binarySearch(Data, borders[pair + 1], 0, Size, false);
         
         if (left_border == -1 || right_border == -1)
             return -1;
